6d63cbe1d9426d063ae18bf0277484fb5420a9f8
ganjiro <girolamomacaluso@gmail.com>
2024-07-29 17:12:03 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ OriginalREDQCodebase/Final_experiments/humanoid/grid_dropQ.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+for env in 'Humanoid'; do
+    for freq in 50000 30000; do
+        for epochs in 20000 10000 50000; do
+            for seed in 0 42 1234 23 666; do
+                # Convert numbers to strings with K notation
+                epochs_str="${epochs%000}K"
+                freq_str="${freq%000}K"
+
+                # Create the exp_name
+                exp_name="${freq_str}_${epochs_str}_dropq"
+
+                # Run the training script
+               python main_o2.py -info lomo/$env/ -env $env-v2 -seed $seed -epochs 100 -gpu_id 0 -method sac -layer_norm 1 \
+              -target_drop_rate 0.1 -exp_name $exp_name -offline_frequency $freq -offline_epochs $epochs  -expectile 0.5 -utd_ratio_online 1
+            done
+        done
+    done
+done
