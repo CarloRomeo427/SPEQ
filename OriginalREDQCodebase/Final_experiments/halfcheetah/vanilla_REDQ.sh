6299ce4e1aa4943e1c2b6ea8ceaea4ae51997308
ganjiro <girolamomacaluso@gmail.com>
2024-08-02 16:31:47 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- OriginalREDQCodebase/Final_experiments/halfcheetah/vanilla_REDQ.sh
+++ OriginalREDQCodebase/Final_experiments/halfcheetah/vanilla_REDQ.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for env in 'Halfcheetah'; do
     for seed in 0 42 1234 23 666; do
-        python main_o2.py -info lomo/$env/ -env $env-v2 -seed $seed -epochs 100 -gpu_id 0 -method redq -layer_norm 1 \
+        python main_o2.py -info lomo/$env/ -env $env-v2 -seed $seed -epochs 300 -gpu_id 0 -method redq -layer_norm 1 \
           -target_drop_rate 0 -exp_name vanilla_redQ -offline_frequency -1 -offline_epochs 0 -offline_dimension 0 -expectile 0.5
     done  
 done
